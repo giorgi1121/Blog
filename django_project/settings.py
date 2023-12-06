aebc28082af0bdb879781fb53baa1f3e09f55931
Giorgi Abashidze <giorgi.abashidze2@gmail.com>
2023-12-07 00:18:35 +0400

    Added password reset

--- django_project/settings.py
+++ django_project/settings.py
@@ -136,3 +136,9 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'profile'
 LOGIN_URL = 'login'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
